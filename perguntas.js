17a3a1d0cb735e141bfa2f8138bd9b35de9c2090
FMBacchi <fernandabacchi2@gmail.com>
2024-11-12 11:03:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,54 @@
+criaCartao(
+    'Literatura',
+    'O que é Romantismo?',
+    'O romantismo é um estilo de época marcado pela subjetividade',
+)
+
+criaCartao(
+    'Educação Física',
+    'Qual é função da educação física?',
+    'A educação física é uma área de conhecimento ligada ao estudo das atividades física, visando o aperfeiçoamentto e desenvolvimento correto dos movimentos corporais e motores',
+)
+
+criaCartao(
+    'Arte',
+    'O que é Arte?',
+    'A arte possui um caráter estético e está intimamente relacionada com as sensações e emoções do indivíduo',
+)
+
+criaCartao(
+    'Biologia',
+    'O que são seres vivos?',
+    'Seres vivos são organismos que possuem características comom capacidade de reprodução, mentabolismo, material genético e capacidade de responder a estímulos.',
+)
+
+criaCartao(
+    'Física',
+    'O que é energia?',
+    'A energia manifesta-se de diferentes formas e está relacionada com a capacidade de produção de ação e movimento em um corpo.',
+)
+
+criaCartao(
+    'Química',
+    'O que são reações químicas?',
+    'As reações químicas são o resultado da transformação que ocorre nas substâncias, onde os átomos rearranjam-se modificando seu estado inicial.',
+)
+
+criaCartao(
+    'Matemática',
+    'O que é matemática',
+    'Matemática é uma ciências que utiliza conceitos e técnicas para formação de conhecimentos abstratos e concretos.',
+)
+
+criaCartao(
+    'Geografia',
+    'O que é Geografia',
+    'Geografia é a ciência cujo objeto de estudo é o espaço geográfico, onde sãom estabelecidas as relações humanas.',
+)
+
+criaCartao(
+    'História',
+    'O que é História',
+    'História é a ciência responsável por estudar os acontecimentos passados.'
+
+)
